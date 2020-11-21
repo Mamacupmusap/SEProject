87980f35cea9be951b3b461646e59f7ad99d1156
MasterPoon <chirapat.ba@ku.th>
2020-11-22 03:00:17 +0700

    alert missing data and change input

--- frontend_johnjud/src/pages_sign_receiver/userprofile3/EditProfile/EditIformation.tsx
+++ frontend_johnjud/src/pages_sign_receiver/userprofile3/EditProfile/EditIformation.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../Profile.css';
 import profileservice from '../ProfileService';
 import {Userinfo} from '../Interface';
+import { First } from 'react-bootstrap/esm/PageItem';
 
 
 const LeftBlockEdit=(prop:any) =>{
@@ -107,6 +108,24 @@ const LeftBlockEdit=(prop:any) =>{
 
 
     const update=() =>{
+      let error_msg = '';
+      if(newFirstName==''){
+        error_msg = error_msg+'FirstName\n';
+      }
+      if(newLastName==''){
+        error_msg = error_msg+'LastName\n';
+      }
+      if(newBirthDay==''){
+        error_msg = error_msg+'BirthDay\n';
+      }
+      if(newAddress==''){
+        error_msg = error_msg+'Address\n';
+      }
+      if(error_msg!=''){
+        alert('Please inform required information : \n' + error_msg);
+        return;
+      }
+
         const newInfoProfile = {
             FirstName: newFirstName,
             LastName: newLastName,
@@ -127,8 +146,13 @@ const LeftBlockEdit=(prop:any) =>{
             <div className='Info'><h5>
             <p className='bold'>FirstName:<input id='Input' value={newFirstName} onChange={(e) => {setNewFirstName(e.target.value);}}></input></p>
             <p className='bold'>LastName:<input id='Input' value={newLastName} onChange={(e) => {setNewLastName(e.target.value);}}></input></p>
-            <p className='bold'>Birthday:<input id='Input' value={newBirthDay} onChange={(e) => {setNewBirthDay(e.target.value);}}></input></p>
-            <p className='bold'>Gender:<input id='Input' value={newGender} onChange={(e) => {setNewGender(e.target.value);}}></input></p>
+            <p className='bold'>Birthday:<input id='Input'type="date" value={newBirthDay} onChange={(e) => {setNewBirthDay(e.target.value);}}></input></p>
+            <p className='bold'>Gender : 
+            <input type='radio' value='male' onChange={(e) => {setNewGender(e.target.value);}}></input>
+            <label>Male</label>
+            <input type='radio' value='female' onChange={(e) => {setNewGender(e.target.value);}}></input>
+            <label>Female</label>
+            </p>
             <p className='bold'>Address:<textarea id='AddressInput' value={newAddress} onChange={(e) => {setNewAddress(e.target.value);}}></textarea></p>
             <button id='ButtonUpdate' onClick={update} >update</button>
             </h5>
