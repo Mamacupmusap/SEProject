422e5e5f0441cd4323db27d84603cbc8509bbc5b
Poomphat <poomphat.p@ku.th>
2020-11-22 19:54:24 +0700

    notiupdate

--- frontend_johnjud/src/pages_sign_donator/chat/ProfileService.tsx
+++ frontend_johnjud/src/pages_sign_donator/chat/ProfileService.tsx
@@ -20,9 +20,48 @@ async function fetchProfileInfo(userId:string): Promise<Userinfo>{
     console.log(name)
     return name;
 }
+async function sendnoti2(a:any,userId:string|undefined, roomid:string): Promise<any|null> {
+    // console.log('userId')
+    // console.log(userId)
+    const res = await fetch(`http://localhost:2000/chat/${userId}/${roomid}/updatereaddate`,{
+        method: 'PATCH',
+        headers : {'Content-Type': 'application/json'},
+        body: JSON.stringify(a),
+    });
+    const ress = await res.json();
+    return ress
+}
+async function getOnenoti(roomid:string, userId2:string): Promise<any>{
+
+    console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    console.log(roomid)
+    console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')
+    console.log(userId2)
+    console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+
+    const res = await fetch(`http://localhost:2000/chat/${userId2}/${roomid}/getmynoti`);
+    const names = await res.json();
+    console.log("nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    console.log(names)
+    return names;
+}
+async function sendnoti(a:any,userId:string|undefined, roomid:string): Promise<any|null> {
+    // console.log('userId')
+    // console.log(userId)
+    const res = await fetch(`http://localhost:2000/chat/${userId}/${roomid}/updatenullnoti`,{
+        method: 'PATCH',
+        headers : {'Content-Type': 'application/json'},
+        body: JSON.stringify(a),
+    });
+    const ress = await res.json();
+    return ress
+}
 export default {
     fetchChatroom,
     fetchProfileInfo,
+    sendnoti2,
+    getOnenoti,
+    sendnoti,
 } 
 
 //
