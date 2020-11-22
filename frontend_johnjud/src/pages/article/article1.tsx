ddb4dd938633c2143222bff2eab0fe382307189b
nichapat0987 <nichapat.ni@ku.th>
2020-11-22 21:33:59 +0700

    css article

--- frontend_johnjud/src/pages/article/article1.tsx
+++ frontend_johnjud/src/pages/article/article1.tsx
@@ -4,25 +4,23 @@ import imgArticle1 from './yerlin-matu-GtwiBmtJvaU-unsplash 1.png'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Navigation2 from '../../Navigation/Navigation2';
+import { Container } from "reactstrap";
 
 const Article1 = () => {
     return (
         <html>
-            <Navigation2/>
-        <head>
-            <title>How to take a good care of your kitten</title>
-        </head>
-        <body className="body1">
-        <div className="container1">
-            <h1 className="headText">How to take a good care of your kitten</h1>
-            <img src={imgArticle1} alt="imgArticle1" className="imageCSS"/>
-            <p className="text-article">การเลี้ยงแมวในคอนโด ห้องพัก หรือในบ้านมักจะมีข้อจำกัดหลาย ๆ อย่าง แตกต่างจากการเลี้ยงแบบปล่อย
-                เพราะมีพื้นที่กว้าง ๆ ให้ได้วิ่งเล่นและเจอสภาพแวดล้อมที่หลากหลายกว่า ดังนั้นแล้ววิธีเลี้ยงเจ้าเหมียวในบ้านจึงค่อนข้างที่จะต้องเอาใจ
-                ใส่มากขึ้นไม่อย่างนั้นอาจทำให้แมวเหมียวอารมณ์ดีกลายเป็นเหมียวขี้เหงาได้ และสำหรับวิธีเลี้ยงแมวในบ้านให้มีความสุข วันนี้กระปุกดอทคอม
-                ได้รวบรวมมาฝากกันแล้วค่ะ
-            </p>
-        </div>
-        </body>
+        <Navigation2/>
+            <Container className="body1">
+        
+                <h1 className="headText">How to take a good care of your kitten</h1>
+                <img src={imgArticle1} alt="imgArticle1" className="imageCSS"/>
+                <p className="text-article">การเลี้ยงแมวในคอนโด ห้องพัก หรือในบ้านมักจะมีข้อจำกัดหลาย ๆ อย่าง แตกต่างจากการเลี้ยงแบบปล่อย
+                    เพราะมีพื้นที่กว้าง ๆ ให้ได้วิ่งเล่นและเจอสภาพแวดล้อมที่หลากหลายกว่า ดังนั้นแล้ววิธีเลี้ยงเจ้าเหมียวในบ้านจึงค่อนข้างที่จะต้องเอาใจ
+                    ใส่มากขึ้นไม่อย่างนั้นอาจทำให้แมวเหมียวอารมณ์ดีกลายเป็นเหมียวขี้เหงาได้ และสำหรับวิธีเลี้ยงแมวในบ้านให้มีความสุข วันนี้กระปุกดอทคอม
+                    ได้รวบรวมมาฝากกันแล้วค่ะ
+                </p>
+        
+            </Container>
         </html>
     );
 }
--- frontend_johnjud/src/pages_sign_donator/article/article1.tsx
+++ frontend_johnjud/src/pages_sign_donator/article/article1.tsx
@@ -4,26 +4,24 @@ import imgArticle1 from './yerlin-matu-GtwiBmtJvaU-unsplash 1.png'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Navigation3 from '../../Navigation/Navigation3';
+import { Container } from "reactstrap";
 
 const Article1 = () => {
-    return (
-        <html>
-            <Navigation3/>
-        <head>
-            <title>How to take a good care of your kitten</title>
-        </head>
-        <body className="body1">
-        <div className="container1">
-            <h1 className="headText">How to take a good care of your kitten</h1>
-            <img src={imgArticle1} alt="imgArticle1" className="imageCSS"/>
-            <p className="text-article">การเลี้ยงแมวในคอนโด ห้องพัก หรือในบ้านมักจะมีข้อจำกัดหลาย ๆ อย่าง แตกต่างจากการเลี้ยงแบบปล่อย
-                เพราะมีพื้นที่กว้าง ๆ ให้ได้วิ่งเล่นและเจอสภาพแวดล้อมที่หลากหลายกว่า ดังนั้นแล้ววิธีเลี้ยงเจ้าเหมียวในบ้านจึงค่อนข้างที่จะต้องเอาใจ
-                ใส่มากขึ้นไม่อย่างนั้นอาจทำให้แมวเหมียวอารมณ์ดีกลายเป็นเหมียวขี้เหงาได้ และสำหรับวิธีเลี้ยงแมวในบ้านให้มีความสุข วันนี้กระปุกดอทคอม
-                ได้รวบรวมมาฝากกันแล้วค่ะ
-            </p>
+    return (   
+        <div>
+        <Navigation3/>
+            <Container className='body1'>
+
+                    <div className="headText">How to take a good care of your kitten</div> 
+                        <img src={imgArticle1} alt="imgArticle1" className="imageCSS"/>
+                        <p className="text-article">การเลี้ยงแมวในคอนโด ห้องพัก หรือในบ้านมักจะมีข้อจำกัดหลาย ๆ อย่าง แตกต่างจากการเลี้ยงแบบปล่อย
+                            เพราะมีพื้นที่กว้าง ๆ ให้ได้วิ่งเล่นและเจอสภาพแวดล้อมที่หลากหลายกว่า ดังนั้นแล้ววิธีเลี้ยงเจ้าเหมียวในบ้านจึงค่อนข้างที่จะต้องเอาใจ
+                            ใส่มากขึ้นไม่อย่างนั้นอาจทำให้แมวเหมียวอารมณ์ดีกลายเป็นเหมียวขี้เหงาได้ และสำหรับวิธีเลี้ยงแมวในบ้านให้มีความสุข วันนี้กระปุกดอทคอม
+                            ได้รวบรวมมาฝากกันแล้วค่ะ
+                        </p>
+              
+            </Container>
         </div>
-        </body>
-        </html>
     );
 }
 
