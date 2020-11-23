bce9e8cb40f5da0bc0230e015df6479bd40b8d3d
Poomphat <poomphat.p@ku.th>
2020-11-23 11:11:55 +0700

    updatechat room1.4

--- frontend_johnjud/src/pages_sign_donator/chat/chatroom.tsx
+++ frontend_johnjud/src/pages_sign_donator/chat/chatroom.tsx
@@ -142,6 +142,9 @@ const Chat= (props:any) =>{
       User:UserId,
       roomid:roomId
     }
+    
+    window.location.reload();
+    window.location.reload();
     profileservice.sendnoti2(a,UserId,roomId)
     // history.push(`http://localhost:3000/receiver/chat/${localStorage.UserId}/${check(userid1,userid2)}/${id}/`)
     
